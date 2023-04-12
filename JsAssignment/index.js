de7301ef8bd5e18e43018d63a7387e3bcebf946a
Zeeshan Choohan <99235041+zeeshan-choohan@users.noreply.github.com>
2023-04-12 13:35:04 +0500

    Add files via upload

--- JsAssignment/index.js
+++ JsAssignment/index.js
@@ -1,8 +1,37 @@
 /*********    TASK 01 ***********/ 
 
+// function closr(addFirst){
+//     return function(addSecond){
+//         console.log(addFirst + addSecond)
+//     }
+// }
+// let innerClosrFun = closr(5);
+// innerClosrFun(5);
 
 
 /*********    TASK 02 ***********/ 
+
+// const arr = [12,4365,76,43,76,98,5,31,4];
+// const pushRecursively = (arr, len = 0, odd = [], even = []) => {
+//    if(len < arr.length){
+//       arr[len] % 2 === 0 ? even.push(arr[len]) : odd.push(arr[len]);
+//       return pushRecursively(arr, ++len, odd, even);
+//    };
+//    return {
+//       odd,
+//       even
+//    }
+// };
+// console.log(pushRecursively(arr));
+
+
+let arr = ['Zeeshan','SMIT','Karachi','Saylani']
+function recursive(arr){
+      return recursive(arr[i])
+}
+recursive()
+
+
 
 /*********    TASK 03 ***********/
 
