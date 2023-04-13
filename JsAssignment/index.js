12ad4453f214883e608c72476987b203f9e3aaa7
Zeeshan Choohan <99235041+zeeshan-choohan@users.noreply.github.com>
2023-04-13 12:42:11 +0500

    Add files via upload

--- JsAssignment/index.js
+++ JsAssignment/index.js
@@ -11,33 +11,76 @@
 
 /*********    TASK 02 ***********/ 
 
-// const arr = [12,4365,76,43,76,98,5,31,4];
-// const pushRecursively = (arr, len = 0, odd = [], even = []) => {
-//    if(len < arr.length){
-//       arr[len] % 2 === 0 ? even.push(arr[len]) : odd.push(arr[len]);
-//       return pushRecursively(arr, ++len, odd, even);
-//    };
-//    return {
-//       odd,
-//       even
-//    }
-// };
-// console.log(pushRecursively(arr));
-
-
-let arr = ['Zeeshan','SMIT','Karachi','Saylani']
-function recursive(arr){
-      return recursive(arr[i])
-}
-recursive()
-
-
-
+// function searchArray(arr, val) {
+    
+//       // Base case: if the array is empty, return false
+//       if (arr.length === 0) {
+//         return false;
+//       }
+      
+//       // Check if the first element of the array is equal to the value
+//       if (arr[0] === val) {
+//         return true;
+//       }
+      
+//       // Recursive case: call the function with the rest of the array
+//       return searchArray(arr.slice(1), val);
+     
+//     }
+//     let arr = [1, 2, 3, 4, 5];
+//     let val = 4;
+//     if (searchArray(arr, val)) {
+//       console.log("Value found!");
+//     } else {
+//       console.log("Value not found.");
+//     }
+        
 /*********    TASK 03 ***********/
+
+// function addParagraph() {
+//   // create a new paragraph element
+//   const newParagraph = document.createElement("p");
+//   let userInp = prompt("Enter any thing !.....")
+
+//   // set the text content of the new paragraph element
+//   newParagraph.textContent = userInp;
+
+//   // get the body element of the document
+//   const body = document.querySelector("body");
+
+//   // add the new paragraph element to the body element
+//   body.appendChild(newParagraph);
+// }
+// addParagraph();
+// addParagraph();
 
 /*********    TASK 04 ***********/ 
 
+// let list = document.getElementById("list")
+// function addList(text){
+//   let newList = document.createElement("li")
+//   newList.textContent = text;
+//   list.append(newList)
+// }
+// addList("hello zeeshan")
+// addList("hello ")
+
 /*********    TASK 05 ***********/
+
+function addParaStyle(element , col){
+  let elements = document.getElementById("elements").getElementsByTagName(element)
+  for(let i = 0; i < elements.length;i++){
+  elements[i].style.color = col;
+
+  }
+}
+addParaStyle('p','green')
+addParaStyle('h1','blue')
+addParaStyle('h2','orange')
+addParaStyle('h3','cadetBlue')
+addParaStyle('h4','purple')
+addParaStyle('h5','red')
+
 
 /*********    TASK 06 ***********/ 
 
